be8fb9aaa319923fe24c10e2f39fcd0826a08ba6
waldbear4 <82853211+waldbear4@users.noreply.github.com>
2021-05-26 14:53:45 +0900

    20210526 Nachmittag Commit

---
+++ mod1.py
@@ -0,0 +1,5 @@
+def add_mod1(a, b):
+    return a+b
+
+def sub_mod1(a, b):
+    return a-b
---
+++ mod2.py
@@ -0,0 +1,8 @@
+PI = 3.14
+
+def add_mod2(a, b):
+    return a+b
+
+class Math_mod2:
+    def solve(self, r):
+        return PI * (r ** 2)
